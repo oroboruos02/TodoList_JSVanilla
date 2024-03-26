d468db8a9821dcf623423609d0c6bca1da632d16
Divar Parra Muñoz <53493035+oroboruos02@users.noreply.github.com>
2024-03-26 16:04:07 -0500

    Init Commit Todo list vanilla

---
+++ todolist.js
@@ -0,0 +1,42 @@
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+const ul = document.querySelector("ul");
+const vacio = document.querySelector(".vacio");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text !== "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+
+    input.value = "";
+    vacio.style.display = "none";
+  }
+});
+
+function addDeleteBtn() {
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "btn-delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+
+    const items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      vacio.style.display = "block";
+    }
+  });
+  return deleteBtn;
+}
